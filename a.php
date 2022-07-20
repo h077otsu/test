6e7e8f430b733fcbac6193cd976a306c16a7fe60
h077otsu <h077otsu@mails.cc.ehime-u.ac.jp>
2022-07-20 10:36:35 +0900

    a.phpの修正

--- a.php
+++ a.php
@@ -3,7 +3,7 @@
   $x = "abc";
   foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    if(strpos($line, $x)){
+    if(strpos($line, $x) !== false){
       echo $line;
     }
   }
